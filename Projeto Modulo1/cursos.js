2824a96ab8abeaa232c7c1fe2897034fa54cb647
tecmessias <50926860+tecmessias@users.noreply.github.com>
2022-11-05 14:36:48 +0000

    Modulo 1 Loja JS
    Site de vendas de cursos, usando um pouco de JS, html e css.

---
+++ Projeto Modulo1/cursos.js
@@ -0,0 +1,74 @@
+const cursos = [
+{
+  isbn: 0,
+  titulo: "HTML5, CSS3, JavaScript",
+  autor: "Maria dos Computadores",
+  preco:250,
+  promocao:150,
+  rating:"***",
+  imgSrc: "./img/curso1.jpg",
+  qtd:0
+},
+{
+  isbn: 1,
+    titulo: "Curso de Comida Vegetariana",
+    autor: "Zé dos Pasteis",
+    preco:200,
+    promocao:50,
+    rating:"***",
+    imgSrc: "./img/curso2.jpg",
+    qtd:0
+},
+{
+  isbn: 2,
+  titulo: "Guitarra para Iniciantes",
+  autor: "Zé da Musica",
+  preco:180,
+  promocao:100,
+  rating:"***",
+  imgSrc: "./img/curso3.jpg",
+  qtd:0
+},
+{
+  isbn: 3,
+  titulo: "A Horte em Casa",
+  autor: "Maria das Verduras",
+  preco:140,
+  promocao:80,
+  rating:"***",
+  imgSrc: "./img/curso4.jpg",
+  qtd:0
+},
+{
+  isbn: 4,
+  titulo: "Decorações Com Produtos Artesanais",
+  autor: "Maria dos Artesanatos",
+  preco:450,
+  promocao:250,
+  rating:"***",
+  imgSrc: "./img/curso5.jpg",
+  qtd:0
+},
+{
+  isbn: 5,
+  titulo: "UX / UI Designer para Principiantes",
+  autor: "Eliza da Internet",
+  preco:650,
+  promocao:649,
+  rating:"***",
+  imgSrc: "./img/curso6.png",
+  qtd:0
+},
+{
+isbn: 5,
+  titulo: "UX / UI Designer para Principiantes",
+  autor: "Eliza da Internet",
+  preco:650,
+  promocao:649,
+  rating:"***",
+  imgSrc: "./img/curso6.png",
+  qtd:0
+},
+
+]
+
